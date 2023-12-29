12d4cf713703e95b6a54e62b10c0558da79555eb
Yasuto1218 <94950313+Yasuto1218@users.noreply.github.com>
2023-12-29 11:06:40 +0900

    修正

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
+import logging 
+
 import pandas as pd
 import numpy as np
 from pydantic import BaseModel, Field
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
+
+logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
-class ConstructionWork(BaseModel):
+# csvのデータからdfを取得。場合によってはDBやCloudから取得する
+import os
+logging.info(os.listdir())
+
+df =  pd.read_csv("./schedule_data/construction_data.csv")
+name_list = df["name"].to_list()
+df.drop(["name", "ID"], axis=1, inplace=True)
+
+
+class ConstructionWorks(BaseModel):
     lgs_board_work: bool = Field(False, description="LGS・ボード工事")  
     sign_work: bool = Field(False, description="サイン工事")  
     temporary_work: bool = Field(False, description="仮設工事")  
@@ -21,29 +34,39 @@ class ConstructionWork(BaseModel):
     miscellaneous_work: bool = Field(False, description="雑工事")  
 
 
-class OutputNameAndID(BaseModel):
+class OutputNameAndIndex(BaseModel):
     construction_name: str = Field(description="工事名")
-    construction_id: str = Field(description="工事ID")  
-    
-@app.get("/get_recommend_schedule_id")
-def get_recommend_schedule_id(ConstructionWork) -> OutputNameAndID:
+    best_cos_similarity: float = Field(description="コサイン類似度")
+
+
+@app.post("/get_recommend_schedule_id")
+def get_recommend_schedule_id(ConstructionWorks: ConstructionWorks) -> OutputNameAndIndex:
     """パラーメータから類似する工程表の番号を取得する
     """
-    # dataclassからDataFrameを作成　-> numpy形式に変更
-    work_dict = {}
-    for cw in ConstructionWork:
-        work_dict[cw[0]] = [cw[1]]
-    work_df = pd.DataFrame(work_dict)
-    work_array = work_df.to_numpy()
+    try:
+        # NOTE:カラム順序を揃えてないので注意。あとでちゃんと揃えましょう。
+        # dataclassからDataFrameを作成　-> numpy形式に変更
+        work_dict = {}
+        for cw in ConstructionWorks:
+            work_dict[cw[0]] = cw[1]
+        work_bec = np.array(list(work_dict.values()))
 
+        # コサイン類似度の最も高いスケジュール名を取得
+        best_name = "該当なし"
+        best_similarity = 0
+        for index_num, data_bec in df.iterrows():
+            data_bec = data_bec.to_numpy()
+            tmp_similarity = _calc_cosine_similarity(work_bec, data_bec)
+            if best_similarity < tmp_similarity:
+                best_similarity = tmp_similarity
+                best_name = name_list[index_num]
 
-    # pd.read_csv("schdule_data.csv")
-
-    # コサイン類似度の最も高いidを列を取得
-    best_name = None
-    _calc_cosine_similatiry(work_array, work_array)
-
-    return OutputNameAndID("工事名", 1)
+        return OutputNameAndIndex(
+            construction_name=best_name, 
+            best_cos_similarity=best_similarity)
+    except Exception as e:
+        logging.error(f"エラーが発生しました: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @app.get("/get_recommend_schedule_param")
@@ -51,10 +74,10 @@ def get_recommend_schedule_param(feature_1: int, feature_2: float, feature_3: in
     return "WIP"
 
 
-def _calc_cosine_similatiry(bec_1: np.array, bec_2: np.array) -> np.array:
+def _calc_cosine_similarity(bec_1: np.array, bec_2: np.array) -> float:
     dot_product = np.dot(bec_1, bec_2)
     norm_1 = np.linalg.norm(bec_1)
     norm_2 = np.linalg.norm(bec_2)
-
     return dot_product / (norm_1 * norm_2)
+
 
